9f64036480def36dfbaf0d4d0dd9c2202d373d44
OttoF77 <ottofreitag@uol.com.br>
2025-08-25 20:52:37 -0300

    Desafio nivel novato do modulo 3, movimentos simples de xadrez.

---
+++ Mod 3/1_exerc_nivel_novato.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main(){
+    //Declaração de variaveis
+    char mov_acima[10] = "cima";
+    char mov_abaixo[10] = "baixo"; 
+    char mov_esquerda[10] = "esquerda";
+    char mov_direita[10] = "direita";
+    int num_mov;
+    int opcao;
+    int status = 1;
+    char menu[300] = "Digite a peça que deseja movimentar:\n 1 - Torre\n 2 - Bispo\n 3 - Rainha\n 4 - Sair do jogo\n";
+    char mov_vertical[10], mov_horizontal[10];
+
+    //Exibição do menu e leitura da opção
+    while(status == 1){
+        printf("%s", menu);
+        scanf("%d", &opcao);
+
+        switch(opcao){
+            case 1:
+                printf("Você escolheu a torre\n");
+                printf("Movimento da torre:\n");
+                num_mov = 5;
+                for(int i = 1; i <= num_mov; i++){
+                    printf("Passo %d: 1 casa para direita\n", i);
+                }
+                break;
+                
+            case 2:
+                printf("Você escolheu o bispo\n");
+                printf("Movimento diagonal:\n");
+                num_mov = 5;
+                for(int i = 1; i <= num_mov; i++){
+                    printf("Passo %d: 1 casa na diagonal para cima e direita\n", i);
+                }
+                break;
+
+            case 3:
+                printf("Você escolheu a rainha\n");
+                printf("Movimento diagonal:\n");
+                num_mov = 8;
+                for(int i = 1; i <= num_mov; i++){
+                    printf("Passo %d: 1 casa para direita e 1 casa para cima\n", i);
+                }
+                break;
+
+            case 4:
+                printf("Saindo do jogo...\n");
+                status = 0;
+                break;
+
+            default:
+                printf("Opção inválida, tente novamente.\n");
+        }
+    }
+
+    return 0;
+}
+
