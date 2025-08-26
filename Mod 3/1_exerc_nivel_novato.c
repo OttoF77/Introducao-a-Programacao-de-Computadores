d64187799a0a9384e01ebd91e1eea2ae95637d19
OttoF77 <ottofreitag@uol.com.br>
2025-08-26 11:23:27 -0300

    Alterações nos movimentos das peças, inclusão dos movimentos do cavalo. Nível aventureiro (intermediário), módulo 3.

--- Mod 3/1_exerc_nivel_novato.c
+++ Mod 3/1_exerc_nivel_novato.c
@@ -9,7 +9,7 @@ int main(){
     int num_mov;
     int opcao;
     int status = 1;
-    char menu[300] = "Digite a peça que deseja movimentar:\n 1 - Torre\n 2 - Bispo\n 3 - Rainha\n 4 - Sair do jogo\n";
+    char menu[300] = "Digite a peça que deseja movimentar:\n 1 - Torre\n 2 - Bispo\n 3 - Rainha\n 4 - Cavalo\n 5 - Sair do jogo\n";
     char mov_vertical[10], mov_horizontal[10];
 
     //Exibição do menu e leitura da opção
@@ -32,7 +32,7 @@ int main(){
                 printf("Movimento diagonal:\n");
                 num_mov = 5;
                 for(int i = 1; i <= num_mov; i++){
-                    printf("Passo %d: 1 casa na diagonal para cima e direita\n", i);
+                    printf("Passo %d: movimento na diagonal 1 casa para cima e 1 casa para direita\n", i);
                 }
                 break;
 
@@ -41,11 +41,25 @@ int main(){
                 printf("Movimento diagonal:\n");
                 num_mov = 8;
                 for(int i = 1; i <= num_mov; i++){
-                    printf("Passo %d: 1 casa para direita e 1 casa para cima\n", i);
+                    printf("Passo %d: 1 casa para esquerda\n", i);
                 }
                 break;
-
             case 4:
+                
+                printf("Você escolheu o cavalo\n");
+                printf("Movimento do cavalo:\n");
+                num_mov = 3;
+                for(int i = 1; i <= num_mov; i++){
+                    for(int j = 1; j <= 1; j++){
+                        if(i <= 2)
+                            printf("Passo %d: 1 casa para baixo\n", i);
+                        else
+                            printf("Passo %d: 1 casa para esquerda\n", i);
+                    }
+                }
+                break;
+                                
+            case 5:
                 printf("Saindo do jogo...\n");
                 status = 0;
                 break;
